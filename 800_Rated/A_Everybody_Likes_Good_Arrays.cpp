6f4768988cd0b5a430cec95c0fd62e83beb73128
Amit Gupta <aryaamit387@gmail.com>
2026-01-25 20:04:47 +0530

    Completed 800 rated problems

---
+++ 800_Rated/A_Everybody_Likes_Good_Arrays.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+
+        for(int i = 0; i < n; i++) 
+            cin >> a[i];
+
+        int ans = 0;
+
+        for(int i = 0 ; i < n-1; i++) {
+            if((a[i] % 2) == (a[i + 1] % 2)) {
+                ans++;
+            }
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
