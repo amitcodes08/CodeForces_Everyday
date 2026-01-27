0a2a5efb3dc3747e2525dd36760b6777fcb3f86c
Amit Gupta <aryaamit387@gmail.com>
2026-01-27 21:30:43 +0530

    fixed issue in d soln of 1076

--- Contests/1076/D_Monster_Game.cpp
+++ Contests/1076/D_Monster_Game.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
 #define ll long long
 #define pb push_back
 #define all(v) v.begin(), v.end()
@@ -9,46 +11,74 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
-void solve() {
-    ll n; cin >> n;
+void solve()
+{
+    ll n;
+    cin >> n;
+
     vector<ll> a(n);
-    for(ll i = 0; i < n; i++) {
+    for (ll i = 0; i < n; i++)
+    {
         cin >> a[i];
     }
 
     vector<ll> b(n);
-    for(ll i = 0; i < n; i++) {
+    for (ll i = 0; i < n; i++)
+    {
         cin >> b[i];
     }
 
-    for(ll i = 1; i < n; i++) {
-        b[i] += b[i-1];
+    vector<ll> prefix(n);
+    prefix[0] = b[0];
+    for (ll i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] + b[i];
     }
 
-    ll j = -1;
+    vector<ll> sorted_a = a;
+    sort(all(sorted_a));
+
     ll ans = 0;
 
-    for(ll i = 0; i < n; i++) {
-        ll cnt = n - i;
-        while(j < n - 1 && b[j + 1] <= cnt) {
-            j++;
+    set<ll> unique_strengths(all(sorted_a));
+
+    for (ll x : unique_strengths)
+    {
+        ll cnt = n - (lower_bound(all(sorted_a), x) - sorted_a.begin());
+        ll levels = 0;
+        ll left = 0, right = n - 1, best = -1;
+        while (left <= right)
+        {
+            ll mid = (left + right) / 2;
+            if (prefix[mid] <= cnt)
+            {
+                best = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
         }
 
-        while(j >= 0 && b[j] > cnt) {
-            j--;
+        if (best != -1)
+        {
+            levels = best + 1;
         }
 
-        ans = max(ans, a[i]* (j + 1));
+        ans = max(ans, x * levels);
     }
 
     cout << ans << nl;
 }
 
-int main() {
+int main()
+{
     fastio;
     int t = 1;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
